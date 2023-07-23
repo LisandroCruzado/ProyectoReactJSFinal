d902a13385672f3228d63466684c48b3ed8ec8d1
Lisandro Cruzado <seveen.cs@msn.com>
2023-07-23 19:34:36 -0300

    PreEntrega2Cruzado

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,43 +1,39 @@
 import React from "react";
 import { useState } from "react";
+import "./ItemCount.css";
 
-const ItemCount = ({stockItems}) => {
+const ItemCount = ({ stockItems }) => {
+  const [counter, setCounter] = useState(1);
+  const [stock] = useState(stockItems);
 
-    const [counter, setCounter] = useState(1);
-    const [stock] = useState(stockItems);
-
-    const incrementarStock = () => {
-        if (counter < stock) {
-            setCounter(counter + 1);
-        } 
+  const incrementarStock = () => {
+    if (counter < stock) {
+      setCounter(counter + 1);
     }
+  };
 
-    const decrementarStock = () => {
-        if (counter > 1) {
-            setCounter(counter - 1)
-        }
-        
+  const decrementarStock = () => {
+    if (counter > 1) {
+      setCounter(counter - 1);
     }
+  };
 
-    return(
-        <div className="container" id="itemcount">
-            <div className="row mb-3">
-                <div className="col-md-2">
-                    <div className="btn-group" role="group" aria-label="Basic outlined example">
-                        <button type="button" className="btn btn-outline-primary" onClick={decrementarStock} >-</button>
-                        <button type="button" className="btn btn-outline-primary">{counter} </button>
-                        <button type="button" className="btn btn-outline-primary" onClick={incrementarStock} >+</button>
-                     </div>
-                </div>
-            </div>
-         <div className="row">
-         <div className="col-md-2">
-            <button type="button" className="btn btn-outline-primary" >Agregar al carrito</button>    
-                
-                </div>
-            </div>
-        </div>  
-    )
-}
+  return (
+    <div className="container" id="itemcount">
+      <div className="row" id="ajuste">
+        <div className="col-md-4 d-flex justify-content-center">
+          <div className="btn-group" role="group" aria-label="Basic outlined example">
+            <button type="button" className="btn btn-outline-dark" onClick={decrementarStock}>-</button>
+            <button type="button" className="btn btn-outline-dark">{counter}</button>
+            <button type="button" className="btn btn-outline-dark" onClick={incrementarStock}>+</button>
+          </div>
+        </div>
+        <div className="col-md-4 d-flex justify-content-center">
+          <button type="button" className="btn btn-dark">Agregar al carrito</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ItemCount;
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ItemCount from '../ItemCount/ItemCount'
-
 const ItemDetail = ({item}) => {
   return (
     <div className='row'>
